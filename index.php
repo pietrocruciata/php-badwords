a456ee711efc0586307db0d6dacd0f13ad9a3b86
pietrocruciata <cruciata9748@gmail.com>
2024-05-06 10:16:12 +0200

    fix the exercise

--- index.php
+++ index.php
@@ -1,21 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
-<main>
-    <form action="./server.php">
-        <p>
-            <label for="paragraph" >messaggio:</label>
-            <input type="text" name="paragraph" id="paragraph" placeholder="scrivi il messaggio">
-        </p>
-       
-        <button>Invia</button>
-    </form>
-</main>
+
+    <main>
+        <form action="./server.php">
+            <p>
+                <label for="paragraph">messaggio:</label>
+                <textarea name="paragraph" id="paragraph" placeholder="scrivi il messaggio"></textarea>
+            </p>
+            <p>
+                <label for="censored">messaggio da censurare:</label>
+                <input type="text" name="censored" id="censored" placeholder="scrivi il messaggio ">
+            </p>
+
+            <button>Invia</button>
+        </form>
+    </main>
 </body>
+
 </html>
--- server.php
+++ server.php
@@ -1,21 +1,28 @@
-<?php 
+<?php
 $paragraph = $_GET['paragraph'];
+$censored = $_GET['censored'];
+
 $paragraph_lenght = strlen($paragraph);
-$new_paragraph = str_replace($paragraph, '***',$paragraph);
+
+$new_paragraph = str_replace($censored, '***', $paragraph, $count);
 $new_paragraph_lenght = strlen($new_paragraph);
-$changed_caracters_lenght = $paragraph_lenght - $new_paragraph_lenght;
+
 
 
 ?>
 
-<div><strong>Messaggio</strong> = <?php echo $paragraph;?></div>
-<div><strong>Lunghezza messaggio </strong>= <?php echo $paragraph_lenght;?></div>
-<div><strong>Nuovo messaggio</strong> = <?php echo $new_paragraph;?></div>
-<div><strong>Lunghezza messaggio</strong> = <?php echo $new_paragraph_lenght;?></div>
-<div><strong>Caratteri cambiati</strong> = <?php echo $changed_caracters_lenght ;?></div>
-
-
-
-
-
-
+<div>
+    <strong>paragrafo</strong> = <?php echo $paragraph; ?>
+</div>
+<div>
+    <strong>Lunghezza paragrafo </strong>= <?php echo $paragraph_lenght; ?>
+</div>
+<div>
+    <strong>Nuovo paragrafo</strong> = <?php echo $new_paragraph; ?>
+</div>
+<div>
+    <strong>Lunghezza nuovo paragrafo</strong> = <?php echo $new_paragraph_lenght; ?>
+</div>
+<div>
+    <strong>Caratteri cambiati</strong> = <?php echo $count; ?>
+</div>
